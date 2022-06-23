c49ccde40b8029d7f3cfdad719972caaf2c255fa
Niklas <niklas.birk.nb@gmail.com>
2022-06-24 01:02:45 +0200

    TEST/SEARCH/UNINFORMED: Add tests to test breadth_first_search algorithm on n eightpuzzle
    - the tests lead to segmentation fault maybe because of too much nodes in the list (?). With better examples where not as much nodes are needed it runs fine.

--- test/search/uninformed/breadth_first_search_test.c
+++ test/search/uninformed/breadth_first_search_test.c
@@ -1,4 +1,114 @@
+#include "../eightpuzzle/dyn_2d_int_array.h"
+#include "../eightpuzzle/example_eightpuzzle.h"
+#include "../eightpuzzle/util.h"
+#include "../../../src/search/uninformed/uninformed_search.h"
+
+void test_should_return_correct_target()
+{
+    Dyn2DIntArray state = {
+            {
+                    {4, 1, 2},
+                    {7, 5, 3},
+                    {8, 6, 0}
+            }
+    };
+    NodeValue stateValue = {
+            .innerValue = &state,
+            .is_target_reached = eightpuzzle_is_target_reached,
+            .successors = eightpuzzle_successors
+    };
+    Node *node = create_node(NULL, &stateValue);
+
+    Dyn2DIntArray target = {
+            {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 0}
+            }
+    };
+    NodeValue targetValue = {
+            .innerValue = &target,
+            .is_target_reached = eightpuzzle_is_target_reached,
+            .successors = eightpuzzle_successors
+    };
+    Node *targetNode = create_node(NULL, &targetValue);
+
+    Node *actual = breadth_first_search(&node, 1, targetNode);
+
+    print_eightpuzzle_solution(actual);
+
+    free_node(node);
+    free_node(targetNode);
+    free_node(actual);
+}
+
+/****************************
+ ********** FAILS ***********
+ ****************************/
+//void test_should_return_correct_target()
+//{
+//    Dyn2DIntArray state = {
+//            {
+//                    {5, 0, 3},
+//                    {2, 1, 6},
+//                    {4, 7, 8}
+//            }
+//    };
+//    NodeValue stateValue = {
+//            .innerValue = &state,
+//            .is_target_reached = eightpuzzle_is_target_reached,
+//            .successors = eightpuzzle_successors
+//    };
+//    Node *node = create_node(NULL, &stateValue);
+//
+//    Dyn2DIntArray target = {
+//            {
+//                    {1, 2, 3},
+//                    {4, 5, 6},
+//                    {7, 8, 0}
+//            }
+//    };
+//    NodeValue targetValue = {
+//            .innerValue = &target,
+//            .is_target_reached = eightpuzzle_is_target_reached,
+//            .successors = eightpuzzle_successors
+//    };
+//    Node *targetNode = create_node(NULL, &targetValue);
+//
+//    Node *actual = breadth_first_search(&node, 1, targetNode);
+//
+//    print_eightpuzzle_solution(actual);
+//
+//    free_node(node);
+//    free_node(targetNode);
+//    free_node(actual);
+//}
+
+//@Test
+//void shouldReturnCorrectTargetCubekNode()
+//{
+//    int[][] state = {
+//            {2, 0, 4},
+//            {6, 7, 1},
+//            {8, 5, 3}
+//    };
+//    var root = new EightPuzzleNode(state);
+//
+//    int[][] targetState = {
+//            {1, 2, 3},
+//            {4, 5, 6},
+//            {7, 8, 0}
+//    };
+//    var expected = new EightPuzzleNode(targetState);
+//
+//    var actual = new BreadthFirstSearch().breadthFirstSearch(List.of(root), expected);
+//
+//    printSolution(actual);
+//}
+
 int main(void)
 {
+    test_should_return_correct_target();
+
     return 0;
 }
